02bc568dcd00fa67fa7fcc5697d64ab3b9e516d6
Louis-Paul Dareau <github@louis-paul.net>
2015-02-03 17:27:09 -0800

    Split the biggest monoliths into smaller ones

--- test.py
+++ test.py
@@ -5,28 +5,19 @@ import random
 from scipy.spatial import Delaunay
 import sys
 
+# Global parameters that are not (yet) automatically computed
 POINTS_COUNT = 150
-POINT_PROPAGATION_X = -1  # Generated later based on the width of the image
-POINT_PROPAGATION_Y = -1  # Generated later based on the height of the image
-POINT_DISTANCE = -1  # Generated later
 EDGE_THRESHOLD = 172
 EDGE_RATIO = .98
 DARKENING_FACTOR = 35
 
 
 def main():
-    global POINT_PROPAGATION_X
-    global POINT_PROPAGATION_Y
-    global POINT_DISTANCE
-
     if len(sys.argv) != 2 or not sys.argv[1]:
         print("Please provide the name of the file to process as an argument")
         exit()
 
     im = Image.open(sys.argv[1])
-    POINT_PROPAGATION_X = im.size[0] / 4
-    POINT_PROPAGATION_Y = im.size[1] / 4
-    POINT_DISTANCE = min(im.size[0], im.size[1]) / 16
 
     # Random point generation
     points = []
@@ -43,25 +34,41 @@ def main():
 
 # generate_random places semi-random point in a list
 def generate_random(im, points):
+    prop_x, prop_y = get_point_propagation(*im.size)
+    point_distance = get_point_distance(*im.size)
     for _ in range(POINTS_COUNT):
-        x = random.randrange(round(
-            (im.size[0] + POINT_PROPAGATION_X) / POINT_DISTANCE
-            )) * POINT_DISTANCE - (POINT_PROPAGATION_X / 2)
-        y = random.randrange(round(
-            (im.size[1] + POINT_PROPAGATION_Y) / POINT_DISTANCE
-            )) * POINT_DISTANCE - (POINT_PROPAGATION_Y / 2)
+        x = random.randrange(round((im.size[0] + prop_x) / point_distance)) * \
+            point_distance - (prop_x / 2)
+        y = random.randrange(round((im.size[1] + prop_y) / point_distance)) * \
+            point_distance - (prop_y / 2)
         points.append([x, y])
+
+
+# get_point_propagation returns an arbirary value as the limit of triangle
+# points outside of the canvas
+def get_point_propagation(width, height):
+    return (width / 4, height / 4)
+
+
+# get_point_distance returns the minimum distance between points (arbitrary
+# value)
+def get_point_distance(width, height):
+    return min(width, height) / 16
 
 
 # generate_edges generates semi-random points in a list, based on an image that
 # was applied an matrix for edge detection
 def generate_edges(im, points):
     im_edges = im.filter(ImageFilter.SHARPEN).filter(ImageFilter.FIND_EDGES)
-    for x, y in product(range(im.size[0]), range(im.size[1])):
-        (r, g, b) = im_edges.getpixel((x, y))
-        if (0.2126*r + 0.7152*g + 0.0722*b) > EDGE_THRESHOLD and \
+    for x, y in product(range(im.size[0] - 1), range(im.size[1] - 1)):
+        if get_grayscale(*im_edges.getpixel((x, y))) > EDGE_THRESHOLD and \
                 random.random() > EDGE_RATIO:
             points.append([x, y])
+
+
+# get_grayscale returns the gray level of a pixel based on its RGB colors
+def get_grayscale(r, g, b):
+    return 0.2126*r + 0.7152*g + 0.0722*b
 
 
 # triangulate generates triangles between points and the list of the colors of
